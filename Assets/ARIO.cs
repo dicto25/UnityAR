7be363c109e44908313bcc9d8463fdd51bb58f2b
a67439371 <a67439371@gmail.com>
2018-09-23 21:54:11 +0800

    Added a new essential feature
    Added a class that allows you to save your words to be recognized by the program

---
+++ Assets/ARIO.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ARobj;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ARIO
+{
+    public class ObjectSaver
+    {
+        private List<ObjectData> dataToSave;
+        private readonly string filepath = "";
+
+        public ObjectSaver()
+        {
+            dataToSave = new List<ObjectData>();
+        }
+
+        public void AddData(string textToRecognize, string objFileName)
+        {
+            ObjectData obj = new ObjectData();
+            obj.Text = textToRecognize;
+            obj.FileName = objFileName;
+            dataToSave.Add(obj);
+        }
+
+        public void AddData(params ObjectData[] datas)
+        {
+            dataToSave.AddRange(datas);
+        }
+
+        public bool SaveData()
+        {
+            try
+            {
+                if (dataToSave.Count != 0)
+                {
+                    ObjectData[] dataArray = dataToSave.ToArray();
+                    XmlSerializer serializer = new XmlSerializer(typeof(ObjectData[]));
+                    using (StreamWriter writer = new StreamWriter(filepath))
+                    {
+                        serializer.Serialize(writer, dataArray);
+                        writer.Close();
+                    }
+                    return true;
+                }
+                else
+                    return false;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+    }
+
+    public class ObjectLoader
+    {
+
+    }
+}
