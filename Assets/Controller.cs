20d344bef7a35797745c6179eaebf512edc4b42f
a67439371 <a67439371@gmail.com>
2018-09-23 15:06:13 +0800

    Added essential feature
    program can now detect images and arrange their representing letters into correct order

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
@@ -6,7 +7,8 @@ using Vuforia;
 public class Controller : MonoBehaviour
 {
     public BoxCollider[] Boxes;
-    public GameObject[] Targets;
+    //public GameObject[] Targets;
+    public string RecognizedString { get; private set; }  //read-only properity
 
     private List<BoxCollider> boxesPresent;
     private BoxCollider[] arrangedBoxes;
@@ -20,53 +22,38 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //try
+        try
         {
             boxesPresent = new List<BoxCollider>();
 
-            foreach (BoxCollider bc in Boxes)
+            foreach (BoxCollider bc in Boxes)  //add all presenting BoxCollider in a list
             {
                 if (bc.enabled)
                     boxesPresent.Add(bc);
             }
 
-            float currentLeast = 9999;
-            arrangedBoxes = new BoxCollider[boxesPresent.Count];
-            for (int i = 0; i < boxesPresent.Count; i++)
-            {
-                int x = 0;
-                for (;x<boxesPresent.Count ; x++)
-                {
-                    GameObject imageObj = Targets[x];
-                    if (imageObj.transform.position.x < currentLeast)
-                    {
-                        currentLeast = imageObj.transform.position.x;
-                        break;
-                    }
-                    else if (i > boxesPresent.Count)
-                        throw new System.Exception("Exception occured in line30 loop");
-                }
-                try
-                {
-                    arrangedBoxes[i] = boxesPresent[x];
-                }
-                catch
-                {
-                    Debug.Log("Index: " + x + ", " + i);
-                }
-            }
+            arrangedBoxes = boxesPresent.ToArray();
 
-            string message = "";
-            foreach (BoxCollider bc in arrangedBoxes)
+            Array.Sort(arrangedBoxes, delegate (BoxCollider b1, BoxCollider b2)  //arrange the BoxColliders accroding to their x position
             {
-                message += bc.name;
+                return b1.transform.position.x.CompareTo(b2.transform.position.x);
+            });
+
+            //TODO: add a y-offset. Targets y-position exceeding the offset would be ignored
+
+            RecognizedString = "";
+            string str = "";
+            foreach(BoxCollider bc in arrangedBoxes)
+            {
+                RecognizedString += bc.name;
+
+                str += ", " + bc.name;  //for debug use
             }
-            Debug.Log(message);
-            //}
-            /*catch(System.Exception e)
-            {
-                Debug.Log(e.Message);
-            }*/
+            Debug.Log(str);  //for debug use
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
         }
     }
 }
