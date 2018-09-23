a8043ed113555950bec3e8c7dcdf354ff6eeed31
a67439371 <a67439371@gmail.com>
2018-09-23 22:40:47 +0800

    Added description to methods

--- Assets/ARIO.cs
+++ Assets/ARIO.cs
@@ -16,6 +16,11 @@ namespace ARIO
             dataToSave = new List<ObjectData>();
         }
 
+        /// <summary>
+        /// Add a datum to the data saver
+        /// </summary>
+        /// <param name="textToRecognize">A word that will be recognized</param>
+        /// <param name="objFileName">The name of the respective .obj file</param>
         public void AddData(string textToRecognize, string objFileName)
         {
             ObjectData obj = new ObjectData();
@@ -24,11 +29,28 @@ namespace ARIO
             dataToSave.Add(obj);
         }
 
+        /// <summary>
+        /// Add multiple datums to the data saver
+        /// </summary>
+        /// <param name="datas"></param>
         public void AddData(params ObjectData[] datas)
         {
             dataToSave.AddRange(datas);
         }
 
+        /// <summary>
+        /// Delete a datums from the data saver
+        /// </summary>
+        /// <param name="selectiveIndex">Starts from 0, indicates which datums would be deleted</param>
+        public void DeleteData(int selectiveIndex)
+        {
+            dataToSave.Remove(dataToSave[selectiveIndex]);
+        }
+
+        /// <summary>
+        /// Write and save the data into a xml file
+        /// </summary>
+        /// <returns></returns>
         public bool SaveData()
         {
             try
